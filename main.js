074d1dc584cbd63746021d56cf1c979f0fa72463
Alex Resiga <resiga_alex@yahoo.com>
2017-12-04 17:01:14 +0200

    change station icon

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" />
 var location_marker;
 var info_window;
 
@@ -14,7 +15,16 @@ function mark_stations(client_id) {
                 var latt = parseFloat(json['route'][i]['latitude']);
                 var longg = parseFloat(json['route'][i]['longitude']);
 
-
+                var imagine ={
+                    path: "M27.648-41.399q0-3.816-2.7-6.516t-6.516-2.7-6.516 2.7-2.7 6.516 2.7 6.516 6.516 2.7 6.516-2.7 2.7-6.516zm9.216 0q0 3.924-1.188 6.444l-13.104 27.864q-.576 1.188-1.71 1.872t-2.43.684-2.43-.684-1.674-1.872l-13.14-27.864q-1.188-2.52-1.188-6.444 0-7.632 5.4-13.032t13.032-5.4 13.032 5",
+                    fillColor: '#217ED7',
+                    fillOpacity: 1,
+                    strokeWeight: 0,
+                    scale: 0.65,
+                    scaledSize: new google.maps.Size(32, 32),
+                    origin: new google.maps.Point(0,0),
+                    anchor: new google.maps.Point(0, 0)
+                }
                 var icon = {
                     url: "logo.png",
                     scaledSize: new google.maps.Size(32, 32),
@@ -25,7 +35,7 @@ function mark_stations(client_id) {
                 var marker = new google.maps.Marker({
                     position: {lat: latt, lng: longg},
                     map: map,
-                    icon: icon,
+                    icon: imagine,
                     title: json['route'][i]['station_name']
                 });
 
