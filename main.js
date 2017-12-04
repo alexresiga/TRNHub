059d0eb487a61badf9babcd0bf93f050a0efee88
Alex Resiga <resiga_alex@yahoo.com>
2017-12-04 16:32:52 +0200

    fixing infowindow again

--- main.js
+++ main.js
@@ -40,7 +40,7 @@ function mark_stations(client_id) {
                         info_window.close();
 
                     info_window = new google.maps.InfoWindow({
-                        content: this.title +"<br><p>Select station for event listing</p>"
+                        content: "<p>"+this.title+"</p>" +"<br><p>Select station for event listing</p>"
                     });
 
                     info_window.open(map, this);
